204ddda30bc184eafa6d63bd28991f6f79cf47a8
qchris101 <qchris101@yahoo.com>
2022-03-02 22:18:02 -0500

    Adjusted url path for change-password

--- api/urls.py
+++ api/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('sign-up/', SignUpView.as_view(), name='sign-up'),
     path('sign-in/', SignInView.as_view(), name='sign-in'),
     path('sign-out/', SignOutView.as_view(), name='sign-out'),
-    path('change-pw/', ChangePasswordView.as_view(), name='change-pw')
+    path('change-password/', ChangePasswordView.as_view(), name='change-pw')
 ]
